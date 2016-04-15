10d17a23aa1ac35c4605b6e379c71ab539cd18f1
Miguel Andrew Aguirre <miguelandrewaguirre@gmail.com>
2016-04-15 14:39:12 -0700

    The add and mul with array function

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+function sum (x,y) {
+  var add = x + y;
+  console.log('The sum of ' + x + ' ' + y + ' is ' + add);
+
+}
+
+
 function mul (x,y) {
   var multiply = x * y;
   console.log('The product of ' + x + ' ' + y + ' is ' + multiply);
 }
+
+
+var sumM = [mul () , sum (),];
